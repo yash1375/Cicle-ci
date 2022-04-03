f7ab4e05c0ba5e404c8ecc31608668b1d8e1d8ac
Abhi <78593493+yash1375@users.noreply.github.com>
2022-04-03 17:10:16 +0530

    Create bot.sh

---
+++ bot.sh
@@ -0,0 +1,23 @@
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get -qq -y install nodejs
+
+git clone https://github.com/botgram/shell-bot --depth 1 shell-bot
+
+cd shell-bot
+
+cat << EOF > config.json
+
+{
+
+  "authToken": ${AUTH},
+
+  "owner": ${OWNER}
+
+ }
+
+EOF
+
+npm install
+
+node server
