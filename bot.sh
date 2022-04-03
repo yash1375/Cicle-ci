9df982699d1f0af93f7fabdb7f5b25be5682ef6f
Abhi <78593493+yash1375@users.noreply.github.com>
2022-04-03 17:24:30 +0530

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -5,19 +5,12 @@ sudo apt-get -qq -y install nodejs
 git clone https://github.com/botgram/shell-bot --depth 1 shell-bot
 
 cd shell-bot
-
 cat << EOF > config.json
-
 {
-
   "authToken": ${AUTH},
-
   "owner": ${OWNER}
-
  }
-
 EOF
-
 npm install
 
 node server
